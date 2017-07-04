9da6fad9f155211de56df364f11d124e179d9f94
Joel Ng <blood_elves88@yahoo.com.sg>
2017-07-04 15:59:57 +0800

    Update to programmed input to have "-demuxer h264es" command

--- thin_client/main.py
+++ thin_client/main.py
@@ -282,51 +282,51 @@ def start_client(ip, port, player_controller_id, *args, **kwargs):
     
     while (is_running):
         # Controllable thin clients
-        event = pygame.event.poll() # Use this if there is no pre-programmed input
-        
-        if (event.type == KEYDOWN or event.type == KEYUP):
-            action = KeyboardButton(session, pygame)                    
-        elif (event.type == pygame.MOUSEMOTION):
-            action = MouseMotion(session, pygame)
-        elif (event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP):
-            action = MouseButton(session, pygame)
-        else:
-            action = Action(session, pygame)
-            
-        #event = pygame.event.poll() # This is needed if sending pre-programmed input
+        #event = pygame.event.poll() # Use this if there is no pre-programmed input
         #
-        #if (player_controller_id == 0):
-        #    time.sleep(0.0333)
-        #    action = MouseMotionXLeft(session, pygame)
-        #elif (player_controller_id == 1):
-        #    time.sleep(0.0333)
+        #if (event.type == KEYDOWN or event.type == KEYUP):
+        #    action = KeyboardButton(session, pygame)                    
+        #elif (event.type == pygame.MOUSEMOTION):
+        #    action = MouseMotion(session, pygame)
+        #elif (event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP):
+        #    action = MouseButton(session, pygame)
+        #else:
         #    action = Action(session, pygame)
-        #    if (time.time() - turnLeft2 > 6):
-        #        action = MouseMotionXLeft(session, pygame)
-        #        turnLeft2 = time.time()
-        #elif (player_controller_id == 2):
-        #    time.sleep(0.0333)
-        #    action = MouseMotionXRight(session, pygame)
-        #    if (time.time() - shootStart3 > 5):
-        #        if (isDown == 0):
-        #            action = MouseButtonLeftDown(session, pygame)
-        #            isDown = 1 - isDown
-        #        else:
-        #            action = MouseButtonLeftUp(session, pygame)
-        #            isDown = 1 - isDown
-        #        shootStart3 = time.time()
-        #elif (player_controller_id == 3):
-        #    time.sleep(0.0333)
-        #    if (counter3 == 100):
-        #        action = KeyboardButtonWUp(session, pygame)
-        #        counter3 = 0
-        #    elif (counter3 < 50):
-        #        action = KeyboardButtonSDown(session, pygame)
-        #    elif (counter3 == 50):
-        #        action = KeyboardButtonSUp(session, pygame)
-        #    elif (counter3 > 50):
-        #        action = KeyboardButtonWDown(session, pygame)
-        #    counter3 += 1        
+            
+        event = pygame.event.poll() # This is needed if sending pre-programmed input
+        
+        if (player_controller_id == 0):
+            time.sleep(0.0333)
+            action = MouseMotionXLeft(session, pygame)
+        elif (player_controller_id == 1):
+            time.sleep(0.0333)
+            action = Action(session, pygame)
+            if (time.time() - turnLeft2 > 6):
+                action = MouseMotionXLeft(session, pygame)
+                turnLeft2 = time.time()
+        elif (player_controller_id == 2):
+            time.sleep(0.0333)
+            action = MouseMotionXRight(session, pygame)
+            if (time.time() - shootStart3 > 5):
+                if (isDown == 0):
+                    action = MouseButtonLeftDown(session, pygame)
+                    isDown = 1 - isDown
+                else:
+                    action = MouseButtonLeftUp(session, pygame)
+                    isDown = 1 - isDown
+                shootStart3 = time.time()
+        elif (player_controller_id == 3):
+            time.sleep(0.0333)
+            if (counter3 == 100):
+                action = KeyboardButtonWUp(session, pygame)
+                counter3 = 0
+            elif (counter3 < 50):
+                action = KeyboardButtonSDown(session, pygame)
+            elif (counter3 == 50):
+                action = KeyboardButtonSUp(session, pygame)
+            elif (counter3 > 50):
+                action = KeyboardButtonWDown(session, pygame)
+            counter3 += 1        
             
         # To toggle mouse grabbing within the window
         if (event.type == KEYUP and event.key == K_ESCAPE):
